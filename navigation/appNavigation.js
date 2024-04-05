6d735990ea6643aea2028f7b0036496a75a02c88
Hoàng Trang <success010280@gmail.com>
2024-04-05 13:16:42 +0700

    add login UI

--- navigation/appNavigation.js
+++ navigation/appNavigation.js
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from '../screen/HomeScreen';
+import { LoginScreen } from '../screen/LoginScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -9,6 +10,7 @@ export default function AppNavigation(){
           <NavigationContainer>
                <Stack.Navigator>
                     <Stack.Screen name="Home" options={{headerShown: false}} component={HomeScreen}/>
+                    <Stack.Screen name="Login" options={{headerShown: false}} component={LoginScreen}/>
                </Stack.Navigator>
           </NavigationContainer>
      )
